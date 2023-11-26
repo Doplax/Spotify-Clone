b876e556119a76ee3e99744fb9a02fecfdefcfc5
Doplax <doplax@gmail.com>
2023-11-26 17:50:00 +0100

    ci: Manage play status on album covers

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -2,18 +2,26 @@ import { usePlayerStore } from "@/store/playerStore"; // <== Global State
 import { Pause, Play } from "./Player";
 
 export function CardPlayButton({ id }) {
-  const { currentSong, isPlaying, setIsPlaying, setCurrentSong } =
+  const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } =
     usePlayerStore((state) => state);
 
   const handleClick = () => {
+    setCurrentMusic({
+        playlist: {
+            id
+        }
+    })
     setIsPlaying(!isPlaying);
   };
+
+  const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id; // Manage play status on album covers
+
   return (
     <button
       onClick={handleClick}
       className="card-play-button rounded-full bg-green-500 p-4"
     >
-      {isPlaying ? <Pause /> : <Play />}
+      {isPlayingPlaylist ? <Pause /> : <Play />}
     </button>
   );
 }
